32da2d55a01ea7164584e031c1e0a985f43a68ed
FatihNitro <FatihNitro@DESKTOP-KERR77G>
2025-06-12 14:15:31 +0300

    Add project files.

---
+++ Pratik_Bebek/Bebek.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratik_Bebek
+{
+     
+    internal class Bebek
+    {
+        public Bebek()
+        {
+            Console.WriteLine("INGAAAAA");
+            DogumTarihi = DateTime.Now;
+        }
+
+        public Bebek(string ad, string soyad)
+        {
+            Console.WriteLine("INGAAAAA");
+            Ad = ad;
+            SoyAd = soyad;
+            DogumTarihi = DateTime.Now;
+        }
+        public string Ad { get; set; }
+        public string SoyAd { get; set; }
+        public DateTime DogumTarihi { get; set; }
+    }
+}
---
+++ Pratik_Bebek/Program.cs
@@ -0,0 +1,14 @@
+﻿using Pratik_Bebek;
+
+Bebek bebek = new Bebek()
+{
+    Ad = "Fatih",
+    SoyAd = "Unal",
+    DogumTarihi = DateTime.Now,
+};
+
+Console.WriteLine("Bebeğin Bilgileri:" +" "+ bebek.Ad +" "+ bebek.SoyAd);
+
+Bebek bebek2 = new Bebek("Selim", "Kazancı");
+
+Console.WriteLine("İkinci Bebeğin Bilgileri: " + " " + bebek2.Ad + " " + bebek.SoyAd + " " + bebek.DogumTarihi);
